f0c6d565771f60f9ac41c5f84ecaa61275b56825
Mahmoud Nabhan <niypoo@gmail.com>
2023-08-28 14:55:10 +0300

    changes

--- lib/views/widgets/textField.widget.dart
+++ lib/views/widgets/textField.widget.dart
@@ -85,6 +85,7 @@ class FlyTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.symmetric(vertical: AppConfigService.to.space!.xxs),
       padding: EdgeInsets.symmetric(horizontal: AppConfigService.to.space!.m),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(AppConfigService.to.radius),
@@ -93,92 +94,90 @@ class FlyTextField extends StatelessWidget {
         ),
         color: color,
       ),
-      child: Center(
-        child: TextFormField(
-          autofocus: autoFocus,
-          focusNode: focusNode,
-          maxLength: maxLength,
-          obscureText: obscureText,
-          enableSuggestions: obscureText ? false : enableSuggestions,
-          autocorrect: obscureText ? false : autoCorrect,
-          controller: controller,
-          validator: validator,
-          maxLines: maxLines,
-          decoration: InputDecoration(
-            alignLabelWithHint: alignLabelWithHint,
-            filled: filled,
-            border: border,
-            labelText: labelText,
-            suffixIcon: suffix != null && suffix is Widget
-                ? suffix
-                : suffix != null
-                    ? Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: AppConfigService.to.space!.s,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              suffix,
-                              style: Get.textTheme.bodySmall!.copyWith(
-                                fontWeight: FontWeight.w600,
-                              ),
+      child: TextFormField(
+        autofocus: autoFocus,
+        focusNode: focusNode,
+        maxLength: maxLength,
+        obscureText: obscureText,
+        enableSuggestions: obscureText ? false : enableSuggestions,
+        autocorrect: obscureText ? false : autoCorrect,
+        controller: controller,
+        validator: validator,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          alignLabelWithHint: alignLabelWithHint,
+          filled: filled,
+          border: border,
+          labelText: labelText,
+          suffixIcon: suffix != null && suffix is Widget
+              ? suffix
+              : suffix != null
+                  ? Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: AppConfigService.to.space!.s,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            suffix,
+                            style: Get.textTheme.bodySmall!.copyWith(
+                              fontWeight: FontWeight.w600,
                             ),
-                          ],
-                        ),
-                      )
-                    : null,
-            prefixIcon: prefix != null && prefix is Widget
-                ? prefix
-                : prefix != null
-                    ? Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: AppConfigService.to.space!.s,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              prefix,
-                              style: Get.textTheme.bodySmall!.copyWith(
-                                fontWeight: FontWeight.w600,
-                              ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : null,
+          prefixIcon: prefix != null && prefix is Widget
+              ? prefix
+              : prefix != null
+                  ? Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: AppConfigService.to.space!.s,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            prefix,
+                            style: Get.textTheme.bodySmall!.copyWith(
+                              fontWeight: FontWeight.w600,
                             ),
-                          ],
-                        ),
-                      )
-                    : null,
-            suffixStyle: Get.textTheme.bodySmall!.copyWith(
-              color: Colors.red,
-            ),
-            prefixStyle: Get.textTheme.bodySmall!.copyWith(
-              color: Colors.red,
-            ),
-            hintText: hintText,
-            hintStyle: textStyle ??
-                Get.textTheme.titleLarge!.copyWith(fontWeight: FontWeight.w100),
-            // contentPadding: EdgeInsets.symmetric(
-            //   vertical: verticalContentPadding ?? AppConfigService.to.space!.xs,
-            //   horizontal:
-            //       horizontalContentPadding ?? AppConfigService.to.space!.xs,
-            // ),
-            errorStyle: Get.textTheme.bodySmall!.copyWith(color: Colors.red),
-            labelStyle: Get.textTheme.titleSmall,
+                          ),
+                        ],
+                      ),
+                    )
+                  : null,
+          suffixStyle: Get.textTheme.bodySmall!.copyWith(
+            color: Colors.red,
           ),
-          style: textStyle ?? Get.textTheme.titleMedium,
-          textAlign: textAlign,
-          onFieldSubmitted: onFieldSubmitted as void Function(String)?,
-          onChanged: onChanged as void Function(String)?,
-          onEditingComplete: onEditingComplete as void Function()?,
-          onSaved: onSaved as void Function(String?)?,
-          onTap: onTap as void Function()?,
-          readOnly: readOnly,
-          textInputAction: textInputAction,
-          keyboardType: textInputType,
-          inputFormatters: inputFormatters,
-          cursorColor: Colors.amberAccent,
+          prefixStyle: Get.textTheme.bodySmall!.copyWith(
+            color: Colors.red,
+          ),
+          hintText: hintText,
+          hintStyle: textStyle ??
+              Get.textTheme.titleLarge!.copyWith(fontWeight: FontWeight.w100),
+          // contentPadding: EdgeInsets.symmetric(
+          //   vertical: verticalContentPadding ?? AppConfigService.to.space!.xs,
+          //   horizontal:
+          //       horizontalContentPadding ?? AppConfigService.to.space!.xs,
+          // ),
+          errorStyle: Get.textTheme.bodySmall!.copyWith(color: Colors.red),
+          labelStyle: Get.textTheme.titleSmall,
         ),
+        style: textStyle ?? Get.textTheme.titleMedium,
+        textAlign: textAlign,
+        onFieldSubmitted: onFieldSubmitted as void Function(String)?,
+        onChanged: onChanged as void Function(String)?,
+        onEditingComplete: onEditingComplete as void Function()?,
+        onSaved: onSaved as void Function(String?)?,
+        onTap: onTap as void Function()?,
+        readOnly: readOnly,
+        textInputAction: textInputAction,
+        keyboardType: textInputType,
+        inputFormatters: inputFormatters,
+        cursorColor: Colors.amberAccent,
       ),
     );
   }
