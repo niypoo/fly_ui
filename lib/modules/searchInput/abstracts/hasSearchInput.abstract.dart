29cb1a967d10a6a6de6ddd19d5282ab327da1d3a
Mahmoud Nabhan <niypoo@gmail.com>
2023-08-28 11:27:26 +0300

    remove constructor

--- lib/modules/searchInput/abstracts/hasSearchInput.abstract.dart
+++ lib/modules/searchInput/abstracts/hasSearchInput.abstract.dart
@@ -2,19 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 abstract class HasSearchInput {
-  // contractor
-  HasSearchInput() {
-    // Handle Search input actions
-    searchTextController.addListener(
-      () {
-        // Value os search input
-        searchIsEmpty.value = searchTextController.text.trim().isEmpty;
-      },
-    );
-
-    print('[HasSearchInput .... ((((instantiated))))]');
-  }
-
   // search text controller
   late TextEditingController searchTextController;
 
