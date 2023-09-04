f8d8c4ca53f2a7caed00e4f63ea52a441f05b702
Mahmoud Nabhan <niypoo@gmail.com>
2023-09-04 15:32:23 +0300

    improve FlyFlashMessageContainer

--- lib/views/widgets/flashMessageContainer.widget.dart
+++ lib/views/widgets/flashMessageContainer.widget.dart
@@ -1,53 +1,41 @@
 import 'package:app_configuration_service/appInfo.config.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:fly_ui/views/widgets/containers/container.widget.dart';
 import 'package:get/get.dart';
 
 class FlyFlashMessageContainer extends StatelessWidget {
   const FlyFlashMessageContainer({
     Key? key,
-    required this.title,
-    required this.child,
+    required this.text,
+    required this.color,
     this.icon,
-    this.bgColor,
-    this.textColor,
   }) : super(key: key);
 
-  final String title;
+  final String text;
   final IconData? icon;
-  final Color? bgColor;
-  final Color? textColor;
-  final String child;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return FlyContainer(
-      color: bgColor,
-      margin: EdgeInsets.only(bottom: AppConfigService.to.space!.m),
-      child: Column(
+    return Container(
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.2),
+        border: Border.all(
+          color: color,
+        ),
+      ),
+      padding: EdgeInsets.all(AppConfigService.to.space!.m),
+      child: Row(
         children: [
-          // Title
-          Row(
-            children: [
-              if (icon != null) Icon(icon, color: textColor),
-              Expanded(
-                child: AutoSizeText(
-                  title,
-                  style: Get.textTheme.labelLarge!.copyWith(
-                    color: textColor,
-                  ),
-                ),
+          if (icon != null) Icon(icon, color: color),
+          Expanded(
+            child: AutoSizeText(
+              text,
+              style: Get.textTheme.labelLarge!.copyWith(
+                color: color,
               ),
-            ],
-          ),
-
-          AutoSizeText(
-            child,
-            style: Get.textTheme.bodyMedium!.copyWith(
-              color: textColor,
             ),
-          )
+          ),
         ],
       ),
     );
