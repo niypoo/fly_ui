3e267a6a92ea18dcf1fe0b77d988f0c3b5318603
Mahmoud Nabhan <niypoo@gmail.com>
2023-09-12 12:59:46 +0300

    remove TextAutoSize

--- lib/views/widgets/pageMessage.widget.dart
+++ lib/views/widgets/pageMessage.widget.dart
@@ -1,5 +1,4 @@
 import 'package:app_configuration_service/appInfo.config.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fly_ui/views/widgets/images/image.widget.dart';
 import 'package:get/get.dart';
@@ -19,7 +18,7 @@ class FlyContainerMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.all(20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -33,12 +32,13 @@ class FlyContainerMessage extends StatelessWidget {
               ),
             ),
           const SizedBox(height: 20),
-          AutoSizeText(
+          Text(
             title,
             style: Get.textTheme.titleLarge,
             textAlign: TextAlign.center,
+            maxLines: 2,
           ),
-          AutoSizeText(
+          Text(
             body,
             style: Get.textTheme.bodyMedium,
             textAlign: TextAlign.center,
