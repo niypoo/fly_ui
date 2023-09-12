ef8cbf00c96f6387bbec6e8f9730ba3d83d6a775
Mahmoud Nabhan <niypoo@gmail.com>
2023-09-12 12:43:09 +0300

    flyPageMessage to FlyContainerMessage

--- lib/views/widgets/pageMessage.widget.dart
+++ lib/views/widgets/pageMessage.widget.dart
@@ -1,51 +1,48 @@
 import 'package:app_configuration_service/appInfo.config.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/widgets.dart';
+import 'package:fly_ui/views/widgets/images/image.widget.dart';
 import 'package:get/get.dart';
 
-class FlyPageMessage extends StatelessWidget {
-  const FlyPageMessage({
+class FlyContainerMessage extends StatelessWidget {
+  const FlyContainerMessage({
     Key? key,
     this.image,
-    this.subtitle,
-    this.title,
+    required this.body,
+    required this.title,
   }) : super(key: key);
 
-  final String? title;
-  final String? subtitle;
+  final String title;
+  final String body;
   final String? image;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(AppConfigService.to.space!.xl),
+      padding: const EdgeInsets.symmetric(vertical: 20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           if (image != null)
-            Padding(
-              padding: EdgeInsets.all(AppConfigService.to.space!.m),
-              child: Center(
-                child: Image.asset(
-                  image!,
-                  width: context.width * 0.4,
-                  height: context.width * 0.4,
-                ),
+            Center(
+              child: FlyImage(
+                url: image,
+                width: context.width * 0.4,
+                height: context.width * 0.4,
               ),
             ),
-          if (title != null)
-            AutoSizeText(
-              title!,
-              style: Get.textTheme.titleLarge!.copyWith(height: 1),
-              textAlign: TextAlign.center,
-            ),
-          if (subtitle != null)
-            AutoSizeText(
-              subtitle!,
-              style: Get.textTheme.bodyMedium,
-              textAlign: TextAlign.center,
-            ),
+          const SizedBox(height: 20),
+          AutoSizeText(
+            title,
+            style: Get.textTheme.titleLarge,
+            textAlign: TextAlign.center,
+          ),
+          AutoSizeText(
+            body,
+            style: Get.textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
         ],
       ),
     );
