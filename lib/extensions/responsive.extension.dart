7894a4b7e22eefcb5eaa0ff3361589f4583b8ac1
Mahmoud Nabhan <niypoo@gmail.com>
2023-10-26 12:00:19 +0300

    Update responsive.extension.dart

--- lib/extensions/responsive.extension.dart
+++ lib/extensions/responsive.extension.dart
@@ -1,16 +1,19 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 extension Responsive on num {
   /// Calculates the height depending on the device's screen size
   ///
   /// Eg: 20.h -> will take 20% of the screen's height
-  double get h => this * Get.height / 100;
+  double get h => this * (isLandscape ? (Get.width / 2) : Get.height) / 100;
 
   /// Calculates the width depending on the device's screen size
   ///
   /// Eg: 20.w -> will take 20% of the screen's width
-  double get w => this * Get.width / 100;
+  double get w => this *  (isLandscape ? (Get.width / 2) : Get.width) / 100;
 
   /// Calculates the sp (Scalable Pixel) depending on the device's screen size
   double get sp => this * (Get.width / 3) / 100;
+
+  bool get isLandscape => Get.mediaQuery.orientation == Orientation.portrait;
 }
