edc3712f9860c2f81ce290dd6d0df2c9e0e59e84
Mahmoud Nabhan <niypoo@gmail.com>
2023-11-04 21:39:35 +0200

    Update drawerButton.widget.dart

--- lib/views/drawer/widgets/drawerButton.widget.dart
+++ lib/views/drawer/widgets/drawerButton.widget.dart
@@ -51,7 +51,7 @@ class FlyDrawerButton extends StatelessWidget {
                   style: Get.textTheme.titleSmall!.copyWith(
                     height: 1.3,
                     fontSize: 10.sp,
-                    fontWeight: FontWeight.w600
+                    fontWeight: FontWeight.w700
                   ),
                   maxLines: 1,
                 ),
