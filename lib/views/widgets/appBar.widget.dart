089c28c258dec56db55a7efac2d676c035f33563
Mahmoud Nabhan <niypoo@gmail.com>
2023-11-08 09:07:11 +0200

    Update appBar.widget.dart

--- lib/views/widgets/appBar.widget.dart
+++ lib/views/widgets/appBar.widget.dart
@@ -1,6 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:fly_ui/extensions/responsive.extension.dart';
+import 'package:fly_ui/views/widgets/buttons/iconButton.widget.dart';
 import 'package:get/get.dart';
 
 class FlyAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -13,7 +14,6 @@ class FlyAppBar extends StatelessWidget implements PreferredSizeWidget {
     this.backgroundColor,
     this.titleTextStyle,
     this.leadingWidth,
-    this.isShrink = false,
     this.centerTitle = false,
     this.padding,
   }) : super(key: key);
@@ -22,12 +22,37 @@ class FlyAppBar extends StatelessWidget implements PreferredSizeWidget {
   final List<Widget> actions;
   final Widget? leading;
   final Color? backgroundColor;
-  final bool isShrink;
   final double? leadingWidth;
   final bool centerTitle;
   final double? padding;
   final TextStyle? titleTextStyle;
   final PreferredSizeWidget? bottom;
+
+  factory FlyAppBar.backButton({
+    String? title,
+    List<Widget> actions = const [],
+    Color? backgroundColor,
+    double? leadingWidth,
+    bool centerTitle = false,
+    double? padding,
+    TextStyle? titleTextStyle,
+    PreferredSizeWidget? bottom,
+  }) =>
+      FlyAppBar(
+        title:title,
+        actions:actions,
+        backgroundColor:backgroundColor,
+        leadingWidth:leadingWidth,
+        centerTitle:centerTitle,
+        padding:padding,
+        titleTextStyle:titleTextStyle,
+        bottom:bottom,
+        leading: FlyIconButton(
+          icon: Icons.arrow_back_ios_new,
+          onPressed: () => Get.back(),
+
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
