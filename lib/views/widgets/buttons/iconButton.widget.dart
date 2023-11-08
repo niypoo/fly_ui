8ab060e3ce002a4eb1637e42af62ec0c7f585cc4
Mahmoud Nabhan <niypoo@gmail.com>
2023-11-08 11:24:46 +0200

    Update iconButton.widget.dart

--- lib/views/widgets/buttons/iconButton.widget.dart
+++ lib/views/widgets/buttons/iconButton.widget.dart
@@ -42,8 +42,8 @@ class FlyIconButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FlyAnimatedRandomFade(
       child: AnimatedContainer(
-        width: 5.h,
-        height: 5.h,
+        width: 22.sp,
+        height: 22.sp,
         duration: AppConfigService.to.duration,
         curve: AppConfigService.to.curve,
         decoration: BoxDecoration(
