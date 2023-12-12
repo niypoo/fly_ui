72b8de5acca972a8aba24c9a00edee175a42c1c4
Mahmoud Nabhan <niypoo@gmail.com>
2023-12-12 11:38:46 +0200

    Update notFoundData.widget.dart

--- lib/views/widgets/notFoundData.widget.dart
+++ lib/views/widgets/notFoundData.widget.dart
@@ -1,23 +1,28 @@
-import 'package:app_configuration_service/appInfo.config.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/widgets.dart';
+import 'package:fly_ui/extensions/responsive.extension.dart';
 import 'package:get/get.dart';
+import 'package:unicons/unicons.dart';
 
 class FlyNotFoundData extends StatelessWidget {
   const FlyNotFoundData({
     Key? key,
     this.title,
-    this.padding,
+    this.icon,
   }) : super(key: key);
 
   final String? title;
-  final double? padding;
+  final IconData? icon;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(padding ?? AppConfigService.to.space!.m),
-      child: AutoSizeText(title ?? 'No data provided yet.'.tr),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon ?? UniconsLine.exclamation, size: 20.sp),
+        SizedBox(height: 5.sp),
+        AutoSizeText(title ?? 'No data provided yet.'.tr),
+      ],
     );
   }
 }
