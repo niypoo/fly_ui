375c2bd8bf23c92379ed813dd15857b652de1dda
Mahmoud Nabhan <niypoo@gmail.com>
2024-01-28 15:51:47 +0200

    Update tableRow.widget.dart

--- lib/views/widgets/tableRow.widget.dart
+++ lib/views/widgets/tableRow.widget.dart
@@ -21,23 +21,29 @@ class FlyTableRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlyInkWell(
-      onTap: onTap,
-      child: Column(
-        mainAxisAlignment: mainAxisAlignment,
-        crossAxisAlignment: crossAxisAlignment,
-        children: [
-          SizedBox(height: 10.sp),
-          Row(children: children),
-          if (bottomHint != null) bottomHint!,
-          Divider(
-            color: Get.theme.cardColor,
-            height: 10.sp,
-            indent: 5.sp,
-            endIndent: 5.sp,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: mainAxisAlignment,
+      crossAxisAlignment: crossAxisAlignment,
+      children: [
+        SizedBox(height: 10.sp),
+        FlyInkWell(
+          onTap: onTap,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(children: children),
+              if (bottomHint != null) bottomHint!,
+            ],
           ),
-        ],
-      ),
+        ),
+        Divider(
+          color: Get.theme.cardColor,
+          height: 10.sp,
+          indent: 5.sp,
+          endIndent: 5.sp,
+        ),
+      ],
     );
   }
 }
