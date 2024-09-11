e756ee49e9eb3604d2094bf0bf3ab05776c19810
Mahmoud Nabhan <niypoo@gmail.com>
2024-09-11 10:34:18 +0300

    Update tileContainer.widget.dart

--- lib/views/widgets/listTile/tileContainer.widget.dart
+++ lib/views/widgets/listTile/tileContainer.widget.dart
@@ -1,58 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:fly_ui/extensions/responsive.extension.dart';
 import 'package:fly_ui/views/widgets/containers/container.widget.dart';
-import 'package:fly_ui/views/widgets/containers/containerOutline.widget.dart';
 import 'package:get/get.dart';
 
 class FlyListTileContainer extends StatelessWidget {
   const FlyListTileContainer({
     Key? key,
     required this.title,
-    required this.value,
+    this.value = '',
     this.onTap,
     this.trailing,
-    this.suffix,
+    this.leading,
   }) : super(key: key);
 
   final String title;
-  final Widget? trailing;
   final String value;
+  final Widget? trailing;
+  final Widget? leading;
   final Function? onTap;
-  final String? suffix;
+
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap as void Function()?,
-      child: FlyContainer(
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 2.sp),
-                    child: Text(
-                      title.toUpperCase(),
-                      style: Get.textTheme.labelLarge!.copyWith(
-                        fontSize: 10.sp,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    '$value ${suffix ?? ''}',
-                    style: Get.textTheme.bodySmall!.copyWith(
-                      fontSize: 9.sp,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            if (trailing != null) trailing!
-          ],
+    return FlyContainer(
+      child: ListTile(
+        onTap: onTap as void Function()?,
+        title: Text(
+          title.toUpperCase(),
+          style: Get.textTheme.labelLarge!.copyWith(
+            fontSize: 10.sp,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        leading: leading,
+        subtitle: Text(
+          value,
+          style: Get.textTheme.bodySmall!.copyWith(
+            fontSize: 9.sp,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        trailing: trailing,
       ),
     );
   }
