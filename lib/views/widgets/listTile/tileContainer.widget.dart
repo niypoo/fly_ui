e08269d57d83ca91bb838103b8f7dae9c4404a81
Mahmoud Nabhan <niypoo@gmail.com>
2025-01-02 12:16:02 +0200

    FlyListTileContainer

--- lib/views/widgets/listTile/tileContainer.widget.dart
+++ lib/views/widgets/listTile/tileContainer.widget.dart
@@ -1,6 +1,9 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:fly_ui/extensions/responsive.extension.dart';
 import 'package:fly_ui/views/widgets/containers/container.widget.dart';
+import 'package:fly_ui/views/widgets/inkWell.widget.dart';
+import 'package:fly_ui/views/widgets/listTile/tileTableRow.widget.dart';
 import 'package:get/get.dart';
 
 class FlyListTileContainer extends StatelessWidget {
@@ -11,6 +14,8 @@ class FlyListTileContainer extends StatelessWidget {
     this.onTap,
     this.trailing,
     this.leading,
+    this.bgColor,
+    this.outline = false,
   }) : super(key: key);
 
   final String title;
@@ -18,31 +23,53 @@ class FlyListTileContainer extends StatelessWidget {
   final Widget? trailing;
   final Widget? leading;
   final Function? onTap;
+  final Color? bgColor;
+  final bool outline;
 
   @override
   Widget build(BuildContext context) {
-    return FlyContainer(
-      padding: const EdgeInsets.all(0),
-      child: ListTile(
-        onTap: onTap as void Function()?,
-        title: Text(
-          title.toUpperCase(),
-          style: Get.textTheme.labelLarge!.copyWith(
-            fontSize: 10.sp,
-            fontWeight: FontWeight.bold,
-          ),
+    return FlyInkWell(
+      onTap: onTap,
+      child: FlyContainer(
+        margin: EdgeInsets.only(bottom: 6.sp),
+        color: bgColor ??
+            (outline ? Get.theme.scaffoldBackgroundColor : Get.theme.cardColor),
+        outline: outline,
+        child: Row(
+          children: [
+            if (leading != null) leading!,
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AutoSizeText(
+                    title,
+                    style: Get.textTheme.titleMedium!.copyWith(
+                      height: 1,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    maxLines: 1,
+                  ),
+                  if (value != null)
+                    Padding(
+                      padding: EdgeInsets.only(top: 3.sp),
+                      child: AutoSizeText(
+                        value!,
+                        style: Get.textTheme.titleMedium!.copyWith(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 11.sp,
+                        ),
+                        maxLines: 3,
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            if (trailing != null) const FlyListTitleLogDivider(),
+            if (trailing != null) trailing!,
+          ],
         ),
-        leading: leading,
-        subtitle: value != null
-            ? Text(
-                value!,
-                style: Get.textTheme.bodySmall!.copyWith(
-                  fontSize: 9.sp,
-                  fontWeight: FontWeight.w300,
-                ),
-              )
-            : null,
-        trailing: trailing,
       ),
     );
   }
