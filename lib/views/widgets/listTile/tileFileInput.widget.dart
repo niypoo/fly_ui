e0e562154da43bcded8ac0fd063d5d3eedeb67ab
Mahmoud Nabhan <niypoo@gmail.com>
2025-01-08 10:31:24 +0200

    FlyFileInputTile

---
+++ lib/views/widgets/listTile/tileFileInput.widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fly_ui/views/widgets/buttons/elevatedButton.widget.dart';
+import 'package:fly_ui/views/widgets/chip.widget.dart';
+import 'package:fly_ui/views/widgets/listTile/inputTileWrap.widget.dart';
+import 'package:get/get.dart';
+import 'package:unicons/unicons.dart';
+
+/// A widget that allows users to input files.
+/// 
+/// This widget provides a user interface for selecting and uploading files.
+/// It can be used in forms or any other place where file input is required.
+class FileInput{
+  final String name;
+  final String path;
+
+  FileInput({required this.name, required this.path});
+}
+
+class FlyFileInputTile extends StatelessWidget {
+  const FlyFileInputTile({
+    Key? key,
+    required this.title,
+    this.subtitle,
+    this.files = const [],
+    this.uploadFun,
+    this.removeFile,
+    this.bgColor,
+    this.child,
+    this.outline = true,
+  }) : super(key: key);
+
+  final String title;
+  final String? subtitle;
+  final List<FileInput> files;
+  final Future Function()? uploadFun;
+  final Future Function(FileInput)? removeFile;
+  final bool outline;
+  final Color? bgColor;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlyInputTileWrap(
+      title: title,
+      subtitle: subtitle,
+      outline: outline,
+      bgColor: bgColor,
+      trailing: FlyElevatedButton.normal(
+        title: 'Upload',
+        icon: UniconsLine.upload,
+        onPressed: uploadFun,
+      ),
+      child: files.isNotEmpty
+          ? Wrap(
+              children: files
+                  .map((file) => FlyChip(
+                        tag: file.name,
+                        backgroundColor: outline
+                            ? null
+                            : Get.theme.scaffoldBackgroundColor,
+                        onRemove:removeFile != null ? () => removeFile!(file) : null,
+                      ))
+                  .toList(),
+            )
+          : null,
+    );
+  }
+}
