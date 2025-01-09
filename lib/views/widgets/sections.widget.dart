584d8659ca369f0cbe669be3c33cc99bab5f272d
Mahmoud Nabhan <niypoo@gmail.com>
2025-01-09 12:18:27 +0200

    Update sections.widget.dart

--- lib/views/widgets/sections.widget.dart
+++ lib/views/widgets/sections.widget.dart
@@ -1,46 +1,46 @@
-import 'package:flutter/material.dart';
-import 'package:fly_ui/extensions/responsive.extension.dart';
-import 'package:get/get.dart';
+// import 'package:flutter/material.dart';
+// import 'package:fly_ui/extensions/responsive.extension.dart';
+// import 'package:get/get.dart';
 
-class FlySection extends StatelessWidget {
-  const FlySection({
-    super.key,
-    required this.child,
-    required this.name,
-    this.divider = false,
-  });
+// class FlySection extends StatelessWidget {
+//   const FlySection({
+//     super.key,
+//     required this.child,
+//     required this.name,
+//     this.divider = false,
+//   });
 
-  final Widget child;
-  final String name;
-  final bool divider;
+//   final Widget child;
+//   final String name;
+//   final bool divider;
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 15.sp),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            name,
-            style: Get.textTheme.titleMedium!
-                .copyWith(fontWeight: FontWeight.w500),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10.sp),
-            child: child,
-          ),
-          if (divider)
-            Divider(
-              color: Get.iconColor!.withOpacity(0.2),
-              height: 10.sp,
-              indent: 10.sp,
-              endIndent: 10.sp,
-            ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: EdgeInsets.only(top: 15.sp),
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         mainAxisSize: MainAxisSize.min,
+//         children: [
+//           Text(
+//             name,
+//             style: Get.textTheme.titleMedium!
+//                 .copyWith(fontWeight: FontWeight.w500),
+//           ),
+//           Padding(
+//             padding: EdgeInsets.all(10.sp),
+//             child: child,
+//           ),
+//           if (divider)
+//             Divider(
+//               color: Get.iconColor!.withOpacity(0.2),
+//               height: 10.sp,
+//               indent: 10.sp,
+//               endIndent: 10.sp,
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+// }
