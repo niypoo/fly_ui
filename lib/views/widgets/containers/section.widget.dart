0dcf38cd8f1b3264b3d31271311e059d83ea74d1
Mahmoud Nabhan <niypoo@gmail.com>
2025-01-09 12:59:34 +0200

    Update section.widget.dart

--- lib/views/widgets/containers/section.widget.dart
+++ lib/views/widgets/containers/section.widget.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:fly_ui/extensions/responsive.extension.dart';
-import 'package:fly_ui/views/widgets/divider.widget.dart';
+// import 'package:fly_ui/views/widgets/divider.widget.dart';
 import 'package:get/get.dart';
 
-class FlySection extends StatelessWidget {
+class FlySection extends StatefulWidget {
   const FlySection({
     super.key,
     required this.title,
     required this.children,
     this.divider = true,
+    this.expanded = true,
   });
 
   final String title;
   final List<Widget> children;
   final bool divider;
+  final bool expanded;
+
+  @override
+  State<FlySection> createState() => _FlySectionState();
+}
+
+class _FlySectionState extends State<FlySection> {
+  bool expanded = true;
+
+  @override
+  void initState() {
+    expanded = widget.expanded;
+    super.initState();
+  }
+
+  // toggle
+  toggle() => setState(() {
+        expanded = !expanded;
+      });
 
   @override
   Widget build(BuildContext context) {
@@ -24,25 +44,25 @@ class FlySection extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
         children: [
-          Padding(
-            padding: EdgeInsets.all(8.sp),
+          GestureDetector(
+            onTap: toggle,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  title,
+                  widget.title,
                   style: Get.textTheme.titleLarge!.copyWith(
                     fontWeight: FontWeight.bold,
                     fontSize: 14.sp,
                   ),
                 ),
-
                 const Icon(Icons.arrow_drop_down)
               ],
             ),
           ),
-          ...children,
-          if (divider) const FlyDivider(),
+
+          if (expanded) ...widget.children,
+          // if (divider) const FlyDivider(),
         ],
       ),
     );
