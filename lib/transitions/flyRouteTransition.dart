791a8b084aee964226ab59fc83bf35f8ceda03ef
Mahmoud Nabhan <niypoo@gmail.com>
2025-01-16 08:23:25 +0200

    Update flyRouteTransition.dart

--- lib/transitions/flyRouteTransition.dart
+++ lib/transitions/flyRouteTransition.dart
@@ -1,8 +1,9 @@
 import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-class FlyRouteTransition extends CustomTransition {
+class FlyFadeThroughTransition extends CustomTransition {
   @override
   Widget buildTransition(
       BuildContext context,
@@ -18,6 +19,74 @@ class FlyRouteTransition extends CustomTransition {
         animation: animation,
         secondaryAnimation: secondaryAnimation,
         child: child,
+      ),
+    );
+  }
+}
+
+class FlySharedAxisTransition extends CustomTransition {
+  @override
+  Widget buildTransition(
+      BuildContext context,
+      Curve? curve,
+      Alignment? alignment,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) {
+    return Align(
+      alignment: Alignment.center,
+      child: SharedAxisTransition(
+        fillColor: Get.theme.scaffoldBackgroundColor,
+        animation: animation,
+        secondaryAnimation: secondaryAnimation,
+        transitionType: SharedAxisTransitionType.scaled,
+        child: child,
+      ),
+    );
+  }
+}
+
+
+class FlyFadeScaleTransition extends CustomTransition {
+  @override
+  Widget buildTransition(
+    BuildContext context,
+    Curve? curve,
+    Alignment? alignment,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return Align(
+      alignment: Alignment.center,
+      child: FadeScaleTransition(
+        animation: animation,
+        child: child,
+      ),
+    );
+  }
+}
+
+class FlyOpenContainerTransition extends CustomTransition {
+  @override
+  Widget buildTransition(
+    BuildContext context,
+    Curve? curve,
+    Alignment? alignment,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return Align(
+      alignment: Alignment.center,
+      child: OpenContainer(
+        closedBuilder: (BuildContext context, void Function() action) {
+          return child;
+        },
+        openBuilder: (BuildContext context,
+            void Function({Object? returnValue}) action) {
+          return child;
+        },
       ),
     );
   }
