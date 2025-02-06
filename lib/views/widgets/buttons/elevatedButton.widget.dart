f9a38e892c1557d17cade45f270c03cdd7ac2b0e
Mahmoud Nabhan <niypoo@gmail.com>
2025-02-06 11:36:48 +0200

    Update elevatedButton.widget.dart

--- lib/views/widgets/buttons/elevatedButton.widget.dart
+++ lib/views/widgets/buttons/elevatedButton.widget.dart
@@ -115,9 +115,9 @@ class FlyElevatedButton extends StatelessWidget {
         vertical: 8.sp,
         horizontal: 12.sp,
       ),
-     disabledBackgroundColor: Colors.grey[50],
-     disabledForegroundColor: Colors.grey[100],
-     disabledIconColor: Colors.grey[100],
+     disabledBackgroundColor: Colors.grey[100],
+     disabledForegroundColor: Colors.grey[50],
+     disabledIconColor: Colors.grey[50],
      disabledMouseCursor: MouseCursor.defer
     );
 
