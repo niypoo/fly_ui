9fdf232b8b255e657561ffc3d884e8d8f2e9d1f0
Mahmoud Nabhan <niypoo@gmail.com>
2025-03-12 21:45:32 +0200

    Update appBar.widget.dart

--- lib/views/widgets/appBar.widget.dart
+++ lib/views/widgets/appBar.widget.dart
@@ -44,12 +44,12 @@ class FlyAppBar extends StatelessWidget implements PreferredSizeWidget {
         scrolledUnderElevation: scrolledUnderElevation,
         backgroundColor: backgroundColor ?? Get.theme.scaffoldBackgroundColor,
         // titleSpacing: titleSpacing ?? (context.isPhone ? 3.sw : 5.sw),
-        leadingWidth: 35.sp,
+        leadingWidth: 30.sp,
         leading: leading ??
             FlyIconButton(
               icon: UniconsLine.multiply,
               onPressed: () => Get.back(),
-              size: 25.sp,
+              size: 22.sp,
             ),
         title: title == null
             ? null
