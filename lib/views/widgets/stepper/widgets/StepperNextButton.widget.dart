459ebea78417de2dc467db3709d8bd93ac10ff88
Mahmoud Nabhan <niypoo@gmail.com>
2025-03-12 23:00:35 +0200

    Update StepperNextButton.widget.dart

--- lib/views/widgets/stepper/widgets/StepperNextButton.widget.dart
+++ lib/views/widgets/stepper/widgets/StepperNextButton.widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:fly_ui/views/widgets/buttons/elevatedButton.widget.dart';
 import 'package:fly_ui/views/widgets/stepper/stepper.controller.dart';
 import 'package:get/get.dart';
-import 'package:localization_service/localization.service.dart';
 
 class FlyStepperNextButton extends StatelessWidget {
   const FlyStepperNextButton({
@@ -23,8 +22,7 @@ class FlyStepperNextButton extends StatelessWidget {
       child: FlyElevatedButton.primary(
         title: buttonTitle,
         onPressed: onTap,
-        icon: icon ?? (LocalizationService.to.isRTL  ? Icons.arrow_back_ios_rounded: Icons.arrow_forward_ios_rounded),
-        iconAlignment: LocalizationService.to.isRTL ? IconAlignment.start : IconAlignment.end,
+        // icon: icon ?? Icons.arrow_forward_ios_rounded,
       ),
     );
   }
@@ -38,10 +36,9 @@ class FlyStepperPreviousButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlyElevatedButton.normal(
-      iconAlignment: LocalizationService.to.isRTL ? IconAlignment.start : IconAlignment.end,
-      icon: LocalizationService.to.isRTL  ? Icons.arrow_forward_ios_rounded: Icons.arrow_back_ios_rounded,
+      // icon: Icons.arrow_back_ios_rounded,
       onPressed: FlyStepperController.to.back,
-      title: 'UI.Previous'.tr,
+      title: 'Previous'.tr,
     );
   }
 }
