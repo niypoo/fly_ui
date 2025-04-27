3283dc66de5a2829170b563f653a491529db325c
Mahmoud Nabhan <niypoo@gmail.com>
2025-04-27 17:00:50 +0300

    Update responsive.extension.dart

--- lib/extensions/responsive.extension.dart
+++ lib/extensions/responsive.extension.dart
@@ -1,26 +1,23 @@
 import 'package:get/get.dart';
 
 extension Responsive on num {
-  
-  bool get isLandscape =>Get.context!.isLandscape ;
-  bool get isTablet =>Get.context!.isTablet; 
+  bool get isLandscape => Get.context!.isLandscape;
+  bool get isTablet => Get.context!.isTablet;
   double get screeWidth => Get.width;
   double get screenHeight => Get.height;
-  int get devicePixelRatio => isTablet ? 12 : 3;
-
+  double get devicePixelRatio => isTablet ? 12 : 8.34;
 
   /// Calculates the height depending on the device's screen size
   double get h => this * screenHeight / 100;
 
   /// Calculates the width depending on the device's screen size
-  double get w => this *  screeWidth / 100;
+  double get w => this * screeWidth / 100;
 
   ///Calculates the sp (Scalable Pixel Half Screen) depending on the device's screen size
-  ///This is useful in cas you split screen in landscape mode 
-  double get sph => this *  (isLandscape ? (screeWidth * 0.5) : screeWidth) / 100;
+  ///This is useful in cas you split screen in landscape mode
+  double get sph =>
+      this * (isLandscape ? (screeWidth * 0.5) : screeWidth) / 100;
 
   /// Calculates the sp (Scalable Pixel) depending on the device's screen size
   double get sp => this * (screenHeight / devicePixelRatio) / 100;
-
-  
 }
