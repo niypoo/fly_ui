3a116448e112ccdd7f3197178f004a9dda8c95c5
Mahmoud Nabhan <niypoo@gmail.com>
2025-04-27 17:53:02 +0300

    Update responsive.extension.dart

--- lib/extensions/responsive.extension.dart
+++ lib/extensions/responsive.extension.dart
@@ -26,40 +26,41 @@ extension Responsive on num {
     if (screenWidth <= 360) {
       return 1.0;
     } else if (screenWidth > 360 && screenWidth <= 411) {
-      return 1.2;
+      return 1.1;
     } else if (screenWidth > 411 && screenWidth <= 480) {
-      return 1.4;
+      return 1.2;
     } else if (screenWidth > 480 && screenWidth <= 540) {
-      return 1.6;
+      return 1.3;
     } else if (screenWidth > 540 && screenWidth <= 600) {
-      return 1.8;
+      return 1.4;
     } else if (screenWidth > 600 && screenWidth <= 720) {
-      return 2.0;
+      return 1.5;
     } else if (screenWidth > 720 && screenWidth <= 1080) {
-      return 2.5;
+      return 1.6;
     } else if (screenWidth > 1080 && screenWidth <= 1440) {
-      return 3.0;
+      return 1.7;
     } else if (screenWidth > 1440 && screenWidth <= 2160) {
-      return 4.0;
+      return 1.8;
     } else if (screenWidth > 2160 && screenWidth <= 2880) {
-      return 5.0;
+      return 1.9;
     } else if (screenWidth > 2880 && screenWidth <= 4320) {
-      return 6.0;
+      return 2.0;
     } else if (screenWidth > 4320 && screenWidth <= 5760) {
-      return 7.0;
+      return 2.2;
     } else if (screenWidth > 5760 && screenWidth <= 8640) {
-      return 8.0;
+      return 2.4;
     } else if (screenWidth > 8640 && screenWidth <= 11520) {
-      return 9.0;
+      return 2.6;
     } else if (screenWidth > 11520 && screenWidth <= 17280) {
-      return 10.0;
+      return 2.8;
     } else if (screenWidth > 17280 && screenWidth <= 23040) {
-      return 11.0;
+      return 3.0;
     } else if (screenWidth > 23040 && screenWidth <= 34560) {
-      return 12.0;
+      return 3.5;
     } else if (screenWidth > 34560 && screenWidth <= 46080) {
-      return 13.0;
+      return 4.0;
     }
+    
     return 2.0; // Default scaling factor
   }
 }
