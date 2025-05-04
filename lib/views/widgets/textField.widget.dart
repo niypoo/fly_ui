aecdaa56a3caf3a4a3b7651ae3393072e9a082e4
Mahmoud Nabhan <niypoo@gmail.com>
2025-05-04 14:49:40 +0300

    add flyTextFieldStyle

--- lib/views/widgets/textField.widget.dart
+++ lib/views/widgets/textField.widget.dart
@@ -102,70 +102,20 @@ class FlyTextField extends StatelessWidget {
         validator: validator,
         maxLines: maxLines,
         decoration: decoration ??
-            InputDecoration(
+            flyTextFieldStyle(
               alignLabelWithHint: alignLabelWithHint,
-              filled: filled,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15.sp),
-                borderSide: BorderSide(
-                  color: borderColor ?? Get.theme.cardColor,
-                ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15.sp),
-                borderSide: BorderSide(
-                  color: borderColor ?? Get.theme.cardColor,
-                ),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15.sp),
-                borderSide: BorderSide(
-                  color: borderColor ?? Get.theme.cardColor,
-                  width: 2.0,
-                ),
-              ),
-              fillColor: color ?? Get.theme.scaffoldBackgroundColor,
-              labelText: labelText,
               isCollapsed: isCollapsed,
-              contentPadding: contentPadding ??
-                  EdgeInsetsDirectional.symmetric(
-                    horizontal: contentPaddingHorizontal ?? 12.sp,
-                    vertical: contentPaddingVertical ?? 12.sp,
-                  ),
-              suffixIcon: suffix.isEmpty
-                  ? null
-                  : Padding(
-                      padding:
-                          EdgeInsetsDirectional.only(end: 5.sp, start: 5.sp),
-                      child: Wrap(
-                        alignment: WrapAlignment.center,
-                        runAlignment: WrapAlignment.center,
-                        runSpacing: 4.sp,
-                        spacing: 4.sp,
-                        children: suffix,
-                      ),
-                    ),
-              prefixIcon: prefix.isEmpty
-                  ? null
-                  : Wrap(
-                      alignment: WrapAlignment.center,
-                      runAlignment: WrapAlignment.center,
-                      children: prefix,
-                    ),
-              suffixStyle: Get.textTheme.labelMedium!.copyWith(
-                color: Colors.red,
-              ),
-              prefixStyle: Get.textTheme.labelMedium!.copyWith(
-                color: Colors.red,
-              ),
+              filled: filled,
+              suffix: suffix,
+              prefix: prefix,
+              borderColor: borderColor,
+              color: color,
+              labelText: labelText,
+              contentPadding: contentPadding,
+              contentPaddingHorizontal: contentPaddingHorizontal,
+              contentPaddingVertical: contentPaddingVertical,
               hintText: hintText,
-              hintStyle: textStyle ?? Get.textTheme.titleSmall!.copyWith(fontWeight: FontWeight.w200),
-              errorStyle: Get.textTheme.labelMedium!.copyWith(
-                color: Colors.red,
-              ),
-              labelStyle: Get.textTheme.titleSmall!.copyWith(
-                fontSize: 11.sp,
-              ),
+              textStyle: textStyle,
             ),
         style: textStyle ?? Get.textTheme.titleSmall!.copyWith(),
         textAlign: textAlign,
@@ -200,4 +150,86 @@ class SuffixText extends StatelessWidget {
       style: Get.textTheme.labelMedium!,
     );
   }
+}
+
+InputDecoration flyTextFieldStyle({
+  bool alignLabelWithHint = false,
+  bool isCollapsed = true,
+  bool filled = true,
+  List<Widget> suffix = const [],
+  List<Widget> prefix = const [],
+  Color? borderColor,
+  Color? color,
+  String? labelText,
+  EdgeInsetsGeometry? contentPadding,
+  double? contentPaddingHorizontal,
+  double? contentPaddingVertical,
+  String? hintText,
+  TextStyle? textStyle,
+}) {
+  return InputDecoration(
+    alignLabelWithHint: alignLabelWithHint,
+    filled: filled,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(15.sp),
+      borderSide: BorderSide(
+        color: borderColor ?? Get.theme.cardColor,
+      ),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(15.sp),
+      borderSide: BorderSide(
+        color: borderColor ?? Get.theme.cardColor,
+      ),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(15.sp),
+      borderSide: BorderSide(
+        color: borderColor ?? Get.theme.cardColor,
+        width: 2.0,
+      ),
+    ),
+    fillColor: color ?? Get.theme.scaffoldBackgroundColor,
+    labelText: labelText,
+    isCollapsed: isCollapsed,
+    contentPadding: contentPadding ??
+        EdgeInsetsDirectional.symmetric(
+          horizontal: contentPaddingHorizontal ?? 12.sp,
+          vertical: contentPaddingVertical ?? 12.sp,
+        ),
+    suffixIcon: suffix.isEmpty
+        ? null
+        : Padding(
+            padding: EdgeInsetsDirectional.only(end: 5.sp, start: 5.sp),
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              runAlignment: WrapAlignment.center,
+              runSpacing: 4.sp,
+              spacing: 4.sp,
+              children: suffix,
+            ),
+          ),
+    prefixIcon: prefix.isEmpty
+        ? null
+        : Wrap(
+            alignment: WrapAlignment.center,
+            runAlignment: WrapAlignment.center,
+            children: prefix,
+          ),
+    suffixStyle: Get.textTheme.labelMedium!.copyWith(
+      color: Colors.red,
+    ),
+    prefixStyle: Get.textTheme.labelMedium!.copyWith(
+      color: Colors.red,
+    ),
+    hintText: hintText,
+    hintStyle: textStyle ??
+        Get.textTheme.titleSmall!.copyWith(fontWeight: FontWeight.w200),
+    errorStyle: Get.textTheme.labelMedium!.copyWith(
+      color: Colors.red,
+    ),
+    labelStyle: Get.textTheme.titleSmall!.copyWith(
+      fontSize: 11.sp,
+    ),
+  );
 }
